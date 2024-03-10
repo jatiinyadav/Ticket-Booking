fa815e7c45052e54040d8d5c3ea9cf6663057ed9
Jatin Yadav <Jatin.Yadav@wuh-group.com>
2024-03-10 13:49:45 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ticket-booking
+
+go 1.22.1
---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titleName = "Ticket Booking Platform"
+	fmt.Printf(`Welcome to %v`, titleName)
+}
